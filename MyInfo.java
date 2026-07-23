6ceb98f4ff6f43bde7fe85d80bfb2a8f588433db
Om More <omkailasmore@gmail.com>
2026-07-24 00:05:27 +0530

    Add files via upload

---
+++ MyInfo.java
@@ -0,0 +1,19 @@
+package Com.om;
+
+public class MyInfo {
+
+		public static void main(String[] args) {
+			String name = "Om";
+			int age = 18;
+			double cgpa = 8.9;
+			char grade = 'A';
+			boolean passed = true;
+			
+			System.out.println("Name: " + name);
+			System.out.println("Age : " + age);
+			System.out.println("CGPA : "+ cgpa);
+			System.out.println("Grade : "+grade);
+			System.out.println(passed);
+		}
+	}
+
