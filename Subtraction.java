65accb07af2d5bbf8c12a3c5ebefe7f19469d07d
Om More <omkailasmore@gmail.com>
2026-07-28 10:22:10 +0530

    Add files via upload

---
+++ Subtraction.java
@@ -0,0 +1,11 @@
+package Com.om;
+
+public class Subtraction {
+	public static void main(String[] aargs) {
+		int a = 50;
+		int b = 20;
+		
+		int sub = a-b;
+		System.out.println("The subtraction of two numbers is : " + sub);
+	}
+}
