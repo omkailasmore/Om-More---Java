936497dc942ffc9b3550ad10d169d4b9395e2a7c
Om More <omkailasmore@gmail.com>
2026-07-28 10:27:50 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,11 @@
+package Com.om;
+
+public class Multiplication {
+public static void main(String[] args) {
+	int a = 5;
+	int b = 10;
+	
+	int multi = a*b;
+	System.out.println("Multiplication of two numbers is : " + a*b);
+}
+}
