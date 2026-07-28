01ca8f3a28dafc3b76ef73959fe3328a0a7afb3f
Om More <omkailasmore@gmail.com>
2026-07-28 10:29:45 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,12 @@
+package Com.om;
+
+public class Division {
+
+public static void main(String[] args) {
+	int a = 500;
+	int b = 10;
+	
+	int div = a/b;
+	System.out.println("Division of two numbers is : " + a/b);
+}
+}
