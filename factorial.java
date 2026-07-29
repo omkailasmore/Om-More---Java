7d44e610eede17146a421bd1d198b12fcc0a40ac
Om More <omkailasmore@gmail.com>
2026-07-29 11:18:05 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,15 @@
+package Com.om;
+
+import java.util.Scanner;
+
+public class factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.println("Hello, " + name);
+    }
+}
+
