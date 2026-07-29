16d1ea879852df58ad64dbbb1878b26ec661f4d1
Om More <omkailasmore@gmail.com>
2026-07-29 11:36:05 +0530

    Add files via upload

---
+++ InfoInput.java
@@ -0,0 +1,14 @@
+package Com.om;
+
+import java.util.Scanner;
+
+public class InfoInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your name : ");
+        String name = sc.nextLine();
+        
+        System.out.println("Hello, " + name);
+    }
+}
