c54139e1d8b4c06db397bf19522369015723b637
Om More <omkailasmore@gmail.com>
2026-08-02 13:37:10 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,29 @@
+package Com.om;
+
+import java.util.Scanner;
+
+public class palindrome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        String cleanedInput = input.replaceAll("\\s+", "").toLowerCase();
+        boolean isPalindrome = true;
+        int length = cleanedInput.length();
+
+        for (int i = 0; i < length / 2; i++) {
+            if (cleanedInput.charAt(i) != cleanedInput.charAt(length - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+        scanner.close();
+    }
+}
